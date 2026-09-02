05a2c6911a105cd86796244017661523011adb21
shruthan650 <shruthan41@gmail.com>
2026-09-02 16:55:52 +0530

    Fixed the improper ordering of dofilter method

--- src/main/java/com/shruthan/jobportalbackend/security/JWTFilter.java
+++ src/main/java/com/shruthan/jobportalbackend/security/JWTFilter.java
@@ -46,8 +46,9 @@ public class JWTFilter extends OncePerRequestFilter{
 				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 				context.setAuthentication(usernamePasswordAuthenticationToken);
 			}
-			filterChain.doFilter(request, response);
 		}
+		
+		filterChain.doFilter(request, response);
 		
 		
 		
