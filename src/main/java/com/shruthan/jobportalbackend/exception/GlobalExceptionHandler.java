bddd66c0603c8af3b4c385a8d9f9689c09c592ca
shruthan650 <shruthan41@gmail.com>
2026-09-04 22:14:54 +0530

    Implemented logging in globalExceptionHandler

--- src/main/java/com/shruthan/jobportalbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shruthan/jobportalbackend/exception/GlobalExceptionHandler.java
@@ -1,20 +1,31 @@
 package com.shruthan.jobportalbackend.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+		
+		logger.warn("Resource Not Found: {}", ex.getMessage());
+		
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(InvalidInputException.class)
 	public ResponseEntity<String> handleInvalidInput(InvalidInputException ex) {
+		
+		logger.warn("Invalid Input: {}", ex.getMessage());
+		
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 }
